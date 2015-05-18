6bc0b91beb55c06cc6ece22798da4d4df6dc3762
Andy de Caussin <thearkofmario@gmail.com>
2015-05-17 19:40:41 -0700

    Intro Sequence, working!
    Don't ask why its slightly brown, i don't know why..
    I also threw in a second material of a lighter grey to break up some of
    the environment!

--- Assets/Scripts/TEMP_menuToLevel.cs
+++ Assets/Scripts/TEMP_menuToLevel.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class TEMP_menuToLevel : MonoBehaviour {
 
+	public GameObject movie;
+	public MovieTexture movieTexture;
 	// Use this for initialization
 	void Start () {
-	
+		GetComponent<Renderer>().material.mainTexture = movieTexture;
 	}
 	
 	// Update is called once per frame
@@ -13,7 +15,10 @@ public class TEMP_menuToLevel : MonoBehaviour {
 
 	}
 
-	void OnMouseDown(){
+	IEnumerator OnMouseDown(){
+		movie.SetActive(true);
+		movieTexture.Play ();
+		yield return new WaitForSeconds(30);
 		Application.LoadLevel (1);
 	}
 
